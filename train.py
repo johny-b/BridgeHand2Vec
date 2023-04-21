28be8941a06fb93d09e429dbd12a44f28fab7b0e
asztyber <sztyber.anna@gmail.com>
2023-04-21 21:34:41 +0200

    train

--- train.py
+++ train.py
@@ -5,4 +5,4 @@ from bh2vec.vectors_net import VectorsNetNorm
 
 net = VectorsNetNorm(n_hid=8)
 train(net, 'data/vectorsnet_train.csv', 'data/vectorsnet_val.csv', epochs=200)
-torch.save(net.state_dict(), "model2.pth")
+torch.save(net.state_dict(), "models/model1.pth")
